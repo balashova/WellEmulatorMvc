6ef05b1c669c9512a9759e279a45c2180834be7d
Andrey <an0648766@mail.ru>
2013-12-19 12:47:34 +0700

    + map item

---
+++ WellEmulator.Models/MapItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WellEmulator.Models
+{
+    [DataContract]
+    public class MapItem
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string HistorianTag { get; set; }
+
+        [DataMember]
+        public string PdgtmTag { get; set; }
+    }
+}
